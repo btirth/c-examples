1d31512f5b584013137c44770380a2f81f90837c
Tirth Bharatiya <emailtirth@gmail.com>
2018-09-22 00:25:35 +0530

    a program that reads an M x N matrix A and prints its elements in spiral order

--- Practical/P17A.C
+++ Practical/P17A.C
@@ -30,38 +30,38 @@ void main()
     while (k < m && l < n)
     {
 	/* Print the first row from the remaining rows */
-	for (i = l; i < n; ++i)
-	{
-	    printf("%3d ", a[k][i]);
-	}
-	k++;
+		for (i = l; i < n; ++i)
+		{
+	    	printf("%3d ", a[k][i]);
+		}
+		k++;
 
 	/* Print the last column from the remaining columns */
-	for (i = k; i < m; ++i)
-	{
-	    printf("%3d ", a[i][n-1]);
-	}
-	n--;
+		for (i = k; i < m; ++i)
+		{
+	    	printf("%3d ", a[i][n-1]);
+		}
+		n--;
 
 	/* Print the last row from the remaining rows */
-	if ( k < m)
-	{
-	    for (i = n-1; i >= l; --i)
-	    {
-		printf("%3d ", a[m-1][i]);
-	    }
-	    m--;
-	}
+		if ( k < m)
+		{
+	    	for (i = n-1; i >= l; --i)
+	    	{		
+				printf("%3d ", a[m-1][i]);
+	    	}
+	    	m--;
+		}
 
 	/* Print the first column from the remaining columns */
-	if (l < n)
-	{
-	    for (i = m-1; i >= k; --i)
-	    {
-		printf("%3d ", a[i][l]);
-	    }
-	    l++;
-	}
+		if (l < n)
+		{
+	    	for (i = m-1; i >= k; --i)
+	    	{
+				printf("%3d ", a[i][l]);
+	    	}
+	    	l++;
+		}
     }
     printf("\n-Tirth Bharatiya");
     getch();
